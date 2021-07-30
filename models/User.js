7c469b03596c2df5a1d54b82ac5891740bff3961
fajar13k <fajar.fj13@gmail.com>
2021-07-30 14:33:39 +0700

    Add User Model

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const moongose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  register_date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = User = mongoose.model('user', UserSchema);
