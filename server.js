9aa579e841b5eb7fda37b937dea14f46c938e08f
fajar13k <fajar.fj13@gmail.com>
2021-07-30 23:22:48 +0700

    Switch using MongoDB Atlas

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const config = require('./config');
+const config = require('config');
 
 // API Routes
 const maskerRoutes = require('./routes/api/masker');
